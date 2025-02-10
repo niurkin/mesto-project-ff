5a53cd8c91ecdce2a3fbbbd4742579c76d865b77
Danila Niurkin <dnurkin@gmail.com>
2025-02-10 17:24:44 +0300

    Реализовано добавление и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList =  document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function addCard(item, action) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    
+    card.querySelector('.card__title').textContent = item.name;
+    card.querySelector('.card__image').src = item.link;
+
+    cardList.append(card);
+
+    cardDeleteButton.addEventListener('click', function(event) {
+        return action(card);
+    });
+}
+
+function removeItem(item) {
+    item.remove();
+}
+
+initialCards.forEach(function(item) {
+    return addCard(item, removeItem);
+});
