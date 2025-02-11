eebda20755e96a52df987a39b3dc6ab95d868787
Danila Niurkin <dnurkin@gmail.com>
2025-02-11 16:12:12 +0300

    Сокращены функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,23 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
 const cardList =  document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function addCard(item, action) {
+function addCard(source, removal) {
+    
     const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
     const cardDeleteButton = card.querySelector('.card__delete-button');
     
-    card.querySelector('.card__title').textContent = item.name;
-    card.querySelector('.card__image').src = item.link;
+    cardTitle.textContent = source.name;
+    cardImage.src = source.link;
 
     cardList.append(card);
 
-    cardDeleteButton.addEventListener('click', function(event) {
-        return action(card);
-    });
+    cardDeleteButton.addEventListener('click', event => removal(card));
 }
 
 function removeItem(item) {
     item.remove();
 }
 
-initialCards.forEach(function(item) {
-    return addCard(item, removeItem);
-});
+initialCards.forEach(item => addCard(item, removeItem));
